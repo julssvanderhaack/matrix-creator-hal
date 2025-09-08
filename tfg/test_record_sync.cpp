823e2c9cd5f0b209912e693614dc89115b9e1437
gcorral <gonzalo.corral@uah.es>
2025-09-08 12:37:46 +0200

    Add test executables for testing the library
    This creates 4 executables test_{record,mqtt}_{sync,async} that test to
    manually test if the library functions are working.
    
    We exercise the read, write_wav and mqtt related functions by
    saving/sending an audio during 5 seconds with gain=0, fs=16000

---
+++ tfg/test_record_sync.cpp
@@ -0,0 +1,42 @@
+#include "../cpp/driver/matrixio_bus.h"
+#include "../cpp/driver/microphone_array.h"
+#include "../cpp/driver/microphone_core.h"
+#include "audio_processor.hpp"
+
+int main() {
+  auto duration = 5;
+  auto frequency = 16000;
+  auto gain = 0;
+  std::string BASE_FILENAME = "test_record_sync";
+
+  auto end_time =
+      std::chrono::steady_clock::now() + std::chrono::seconds(duration);
+
+  std::cerr << "Testing record sync with: " << frequency
+            << " Hz, gain = " << gain << " during " << duration << " s "
+            << " with filenames " << BASE_FILENAME << std::endl;
+
+  // Inicializar bus MATRIX
+  matrix_hal::MatrixIOBus bus;
+  if (!bus.Init()) {
+    return 1;
+  }
+
+  // Mic array
+  matrix_hal::MicrophoneArray mic_array(false);
+  matrix_hal::MicrophoneCore mic_core(mic_array);
+  mic_array.Setup(&bus);
+  mic_array.SetSamplingRate(frequency);
+
+  if (gain > 0) {
+    mic_array.SetGain(gain);
+  }
+
+  mic_array.ShowConfiguration();
+  mic_core.Setup(&bus);
+
+  while (duration == 0 || std::chrono::steady_clock::now() < end_time) {
+    auto b = capture_audio_sync(&mic_array);
+    record_all_channels_wav_sync(&mic_array, b, BASE_FILENAME);
+  }
+}
