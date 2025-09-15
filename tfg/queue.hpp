446a5ab8ed10cfb59fd2508901b41ce2e5cf0148
Julio Albisua <julio.albisua@edu.uah.es>
2025-09-15 19:25:03 +0200

    Add a running bool to the async queue.
    Using wait_pop() is desirable, as it can suspend the reading thread when
    the producer is behind the consumer, but with the current code we can
    have a deadlock if we change running to false while the thread is
    waiting.
    
    This is because no more data is produced, so the condition variable is
    not notified, and even if we have a sudden wakeup the condition
    !queue.empty() is not going to trigger, so the thread is going to go
    back to sleep.
    
    There are two possible solutions:
    - Don't use wait_pop(), and do busy looping with pop().
    - Introduce a function to notify to the queue that we are not going to
      do more work.
    
    We adopt the second solution, but now we have to notify if we have more
    data or not so use a return value of 1, to indicate that we are running
    and we have more data, -1 to signal that we are not running, but we are
    returning one of the leftovers elements in the queue and 0 to tell the
    caller that we are no longer running and we have no more data.

--- tfg/queue.hpp
+++ tfg/queue.hpp
@@ -9,6 +9,8 @@
 
 template<typename T>
 class SafeQueue {
+public:
+    std::atomic_bool run_async{true};
 private:
     std::queue<T> queue_;
     std::mutex mutex_;
@@ -30,17 +32,56 @@ public:
         return true;
     }
 
-    void wait_pop(T& item) {
+    int wait_pop(T& item) {
         std::unique_lock<std::mutex> lock(mutex_);
-        cond_.wait(lock, [&] { return !queue_.empty(); });
+        // Note that we can have spureus wakeups, in addition to notified ones.
+        // That means that this may not work perfectly, but it should generally
+        // work. And even if this wakes up we have the return values to
+        // communicate the situation.
+        cond_.wait(lock, [&] {
+            // Exit the wait when the queue has data or we are no longer
+            // running. If we don't use run_async, we can have a deadlock if we
+            // are waiting and the threads are no longer producing (running =
+            // false;). This way we wake up when running == false and return 0.
+            return queue_.empty() == false || run_async == false;
+        });
+        if(queue_.empty()) {
+            // If the queue if empty, return 0 to indicate we have no more values.
+            // This is only possible if we are woken up with run_async == false.
+            return 0;
+        }
         item = queue_.front();
         queue_.pop();
+        if(!run_async) {
+            // Return -1 if we have stopped producing, but we have more data in the queue.
+            return -1;
+        }
+        // Return 1 if we are producing more values, and we have data in the queue.
+        return 1;
     }
 
     bool empty() {
         std::unique_lock<std::mutex> lock(mutex_);
         return queue_.empty();
     }
+
+    void start_async() {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            run_async = true;
+        }
+        cond_.notify_all();
+    }
+
+    void stop_async() {
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            run_async = false;
+        }
+        cond_.notify_all();
+    }
+
+
 };
 
 struct AudioBlock {
